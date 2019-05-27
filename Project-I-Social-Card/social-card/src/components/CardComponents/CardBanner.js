1651f9a5a6faa1cc18d9846f48ebd2e73764134e
Winnie Song <winniesong0121@gmail.com>
2019-05-27 19:48:30 -0400

    finished MVP

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,9 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+
+const CardBanner = props => {
+  return <div className="card-banner" />;
+};
+
+export default CardBanner;
+
